bbc136f54d0dfa08676351ec456d03f958b7adb6
Cameron Angliss <cameronangliss99@gmail.com>
2025-08-14 00:03:51 -0500

    shortDesc is generally more useful if you want to do embeddings with it

--- vgc_bench/scrape_data.py
+++ vgc_bench/scrape_data.py
@@ -18,12 +18,16 @@ def update_desc_embeddings(url: str, file: str, extras: dict[str, dict[str, str]
         js_literal = js_text[i:-1]
         json_text = re.sub(r"([{,])([a-zA-Z0-9_]+)(:)", r'\1"\2"\3', js_literal)
         file += "on"
-    dex = {k: v for k, v in {**extras, **json.loads(json_text)}.items() if "desc" in v}
+    dex = {
+        k: v["shortDesc"]
+        for k, v in {**extras, **json.loads(json_text)}.items()
+        if "shortDesc" in v
+    }
     warnings.simplefilter(action="ignore", category=FutureWarning)
     transformer = SentenceTransformer("paraphrase-mpnet-base-v2")
-    embeddings = transformer.encode([a["desc"] for a in dex.values()])
     pca = PCA(100)
-    reduced_embeddings = pca.fit_transform(embeddings).tolist()  # type: ignore
+    embeddings = transformer.encode(list(dex.values()))
+    reduced_embeddings = pca.fit_transform(embeddings).tolist()
     with open(f"data/{file}", "w") as f:
         json.dump(dict(zip(dex.keys(), reduced_embeddings)), f)
 
@@ -34,17 +38,19 @@ if __name__ == "__main__":
     update_desc_embeddings(
         "https://play.pokemonshowdown.com/data",
         "abilities.js",
-        extras={"null": {"desc": "null"}, "": {"desc": "empty"}},
+        extras={"null": {"shortDesc": "null"}, "": {"shortDesc": "empty"}},
     )
     update_desc_embeddings(
         "https://play.pokemonshowdown.com/data",
         "items.js",
         extras={
-            "null": {"desc": "null"},
-            "": {"desc": "empty"},
-            "unknown_item": {"desc": "unknown item"},
+            "null": {"shortDesc": "null"},
+            "": {"shortDesc": "empty"},
+            "unknown_item": {"shortDesc": "unknown item"},
         },
     )
     update_desc_embeddings(
-        "https://play.pokemonshowdown.com/data", "moves.js", extras={"no move": {"desc": "no move"}}
+        "https://play.pokemonshowdown.com/data",
+        "moves.js",
+        extras={"no move": {"shortDesc": "no move"}},
     )
