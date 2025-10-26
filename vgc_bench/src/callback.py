9c09969c0e8a3b1441a5e7f9306964ddf5abcf85
Cameron Angliss <cameronangliss99@gmail.com>
2025-10-26 14:30:48 -0500

    clean up

--- vgc_bench/src/callback.py
+++ vgc_bench/src/callback.py
@@ -31,10 +31,9 @@ class Callback(BaseCallback):
         chooses_on_teampreview: bool,
     ):
         super().__init__()
+        self.num_teams = num_teams
         self.learning_style = learning_style
         self.behavior_clone = behavior_clone
-        self.run_id = run_id
-        self.num_teams = num_teams
         self.run_ident = "".join(
             [
                 "-bc" if behavior_clone else "",
@@ -44,17 +43,15 @@ class Callback(BaseCallback):
                 "-xt" if not chooses_on_teampreview else "",
             ]
         )[1:]
-        if not os.path.exists(f"results{run_id}/logs-{self.run_ident}"):
-            os.mkdir(f"results{run_id}/logs-{self.run_ident}")
+        self.log_dir = f"results{run_id}/logs-{self.run_ident}"
+        self.save_dir = f"results{run_id}/saves-{self.run_ident}/{self.num_teams}-teams"
+        if not os.path.exists(self.log_dir):
+            os.mkdir(self.log_dir)
         self.payoff_matrix: npt.NDArray[np.float32]
         self.prob_dist = None
         if self.learning_style == LearningStyle.DOUBLE_ORACLE:
-            if os.path.exists(
-                f"results{run_id}/logs-{self.run_ident}/{self.num_teams}-teams-payoff-matrix.json"
-            ):
-                with open(
-                    f"results{run_id}/logs-{self.run_ident}/{self.num_teams}-teams-payoff-matrix.json"
-                ) as f:
+            if os.path.exists(f"{self.log_dir}/{self.num_teams}-teams-payoff-matrix.json"):
+                with open(f"{self.log_dir}/{self.num_teams}-teams-payoff-matrix.json") as f:
                     self.payoff_matrix = np.array(json.load(f))
             else:
                 self.payoff_matrix = np.array([[0.5]])
@@ -121,26 +118,15 @@ class Callback(BaseCallback):
             win_rate = self.compare(self.eval_agent, self.eval_opponent, 1000)
             self.model.logger.record("train/eval", win_rate)
         if not self.behavior_clone:
-            self.model.save(
-                f"results{self.run_id}/saves-{self.run_ident}/{self.num_teams}-teams/{self.model.num_timesteps}"
-            )
+            self.model.save(f"{self.save_dir}/{self.model.num_timesteps}")
         else:
             try:
-                saves = [
-                    int(file[:-4])
-                    for file in os.listdir(
-                        f"results{self.run_id}/saves-{self.run_ident}/{self.num_teams}-teams"
-                    )
-                    if int(file[:-4]) >= 0
-                ]
+                saves = [int(f[:-4]) for f in os.listdir(self.save_dir) if int(f[:-4]) >= 0]
             except FileNotFoundError:
                 raise FileNotFoundError("behavior_clone on, but no model initialization found")
             assert len(saves) > 0
         if self.learning_style == LearningStyle.EXPLOITER:
-            policy = PPO.load(
-                f"results{self.run_id}/saves-{self.run_ident}/{self.num_teams}-teams/-1",
-                device=self.model.device,
-            ).policy
+            policy = PPO.load(f"{self.save_dir}/-1", device=self.model.device).policy
             for i in range(self.model.env.num_envs):
                 self.model.env.env_method("set_opp_policy", policy, indices=i)
 
@@ -150,17 +136,12 @@ class Callback(BaseCallback):
         if self.behavior_clone:
             self.model.policy.actor_grad = self.model.num_timesteps >= save_interval  # type: ignore
         if self.learning_style in [LearningStyle.FICTITIOUS_PLAY, LearningStyle.DOUBLE_ORACLE]:
-            policy_files = os.listdir(
-                f"results{self.run_id}/saves-{self.run_ident}/{self.num_teams}-teams"
-            )
+            policy_files = os.listdir(self.save_dir)
             policies = random.choices(
                 policy_files, weights=self.prob_dist, k=self.model.env.num_envs
             )
             for i in range(self.model.env.num_envs):
-                policy = PPO.load(
-                    f"results{self.run_id}/saves-{self.run_ident}/{self.num_teams}-teams/{policies[i]}",
-                    device=self.model.device,
-                ).policy
+                policy = PPO.load(f"{self.save_dir}/{policies[i]}", device=self.model.device).policy
                 self.model.env.env_method("set_opp_policy", policy, indices=i)
 
     def _on_rollout_end(self):
@@ -169,22 +150,17 @@ class Callback(BaseCallback):
             self.model.logger.record("train/eval", win_rate)
             if self.learning_style == LearningStyle.DOUBLE_ORACLE:
                 self.update_payoff_matrix()
-            self.model.save(
-                f"results{self.run_id}/saves-{self.run_ident}/{self.num_teams}-teams/{self.model.num_timesteps}"
-            )
+            self.model.save(f"{self.save_dir}/{self.model.num_timesteps}")
 
     def _on_training_end(self):
         self.model.logger.dump(self.model.num_timesteps)
 
     def update_payoff_matrix(self):
-        policy_files = os.listdir(
-            f"results{self.run_id}/saves-{self.run_ident}/{self.num_teams}-teams"
-        )
+        policy_files = os.listdir(self.save_dir)
         win_rates = np.array([])
         for p in policy_files:
             self.eval_agent2.policy = PPO.load(
-                f"results{self.run_id}/saves-{self.run_ident}/{self.num_teams}-teams/{p}",
-                device=self.model.device,
+                f"{self.save_dir}/{p}", device=self.model.device
             ).policy
             win_rate = self.compare(self.eval_agent, self.eval_agent2, 1000)
             win_rates = np.append(win_rates, win_rate)
@@ -192,10 +168,7 @@ class Callback(BaseCallback):
         win_rates = np.append(win_rates, 0.5)
         self.payoff_matrix = np.concat([self.payoff_matrix, win_rates.reshape(1, -1)], axis=0)
         self.prob_dist = Game(self.payoff_matrix).linear_program()[0].tolist()
-        with open(
-            f"results{self.run_id}/logs-{self.run_ident}/{self.num_teams}-teams-payoff-matrix.json",
-            "w",
-        ) as f:
+        with open(f"{self.log_dir}/{self.num_teams}-teams-payoff-matrix.json", "w") as f:
             json.dump(
                 [
                     [round(win_rate, 2) for win_rate in win_rates]
